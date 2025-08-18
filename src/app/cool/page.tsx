80af7224fb655a2f25343c5f5554bdce29314e3e
Indy1131 <62616129+Indy1131@users.noreply.github.com>
2025-08-17 21:58:27 -0400

    finished cool page

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Kewl() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
@@ -7,7 +9,12 @@ export default function Kewl() {
       Feel free to put links, images, whatever! 
       Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
       */}
-      TODO!
+      <Image src="/hk.jpg" alt="pic of lantau island" width={500} height={100} />
+      <h1 className="text-lg px-10">
+        I'm Nick and one cool thing I did was that I went to study abroad in
+        China last summer. I like hiking and biking (both of which I got to do a
+        lot of in China)
+      </h1>
     </div>
   );
 }
